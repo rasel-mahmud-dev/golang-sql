cd2a2068e09f94162cf459ef580e8e1ca436e79a
rasel-mahmud-dev <rasel.mahmud.dev@gmail.com>
2022-03-20 01:58:37 +0600

    in

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
  "database/sql"
- "encoding/json"
+ // "encoding/json"
  "fmt"
  _ "github.com/go-sql-driver/mysql"
  "github.com/rasel-mahmud-dev/golang-sql/config"
@@ -10,10 +10,145 @@ import (
  "os"
 )
 
+var connectionStr string
+//
+// func InitialCreateAllTable() {
+//  db, err := sql.Open("mysql", connectionStr)
+//  if err != nil {
+//   panic(err)
+//  }
+//  defer db.Close()
+//
+//  userTableSql := `
+// 		CREATE TABLE IF NOT EXISTS users (
+// 		  id INT PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
+// 		  username VARCHAR(40) NOT NULL,
+// 		  email VARCHAR(50) NOT NULL UNIQUE,
+// 		  password VARCHAR(100) NOT NULL,
+// 		  avatar VARCHAR(100) NULL DEFAULT NULL,
+// 		  created_at DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+// 		  role ENUM('admin', 'user') NOT NULL DEFAULT 'user'
+// 		)`
+//
+//  res, err := db.Exec(userTableSql)
+//  if err != nil {
+//   fmt.Println(err)
+//  } else {
+//   fmt.Println("Users Table Created ")
+//   _ = res
+//  }
+//
+//  res, err = db.Exec(`
+// 		CREATE TABLE IF NOT EXISTS products (
+// 		  id INT PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
+// 		  title VARCHAR(255) NOT NULL,
+// 		  description LONGTEXT NULL,
+// 		  price DOUBLE,
+// 		  author_id INT NOT NULL,
+// 		  brand_id INT DEFAULT 0,
+// 			cover VARCHAR(255) DEFAULT '',
+// 			tags json DEFAULT NULL,
+// 			attributes json DEFAULT NULL,
+// 		  created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+// 			updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+// 		)`)
+//  if err != nil {
+//   fmt.Println(err, "-----")
+//  } else {
+//   fmt.Println("Products Table Created ")
+//  }
+//
+//  res, err = db.Exec(`
+// 		CREATE TABLE IF NOT EXISTS brands (
+// 		  id INT PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
+// 		  name VARCHAR(255) NOT NULL,
+// 		  created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+// 		)`)
+//  if err != nil {
+//   fmt.Println(err, "-----")
+//  } else {
+//   fmt.Println("Brands Table Created ")
+//  }
+// }
+//
+// func InitialData()  {
+//  db, err := sql.Open("mysql", connectionStr)
+//  if err != nil {
+//   panic(err)
+//  }
+//  defer db.Close()
+//
+//  _, err = db.Exec(`
+// 			INSERT INTO users(username, email, role, password, avatar)
+// 			VALUES
+// 			(
+// 				'rasel',
+// 				'rasel@gmail.com',
+// 				'admin',
+// 				'$2a$08$Tj9kveUf9GzZCn4l70wqdeh8Uq.FK2T6sYXcGGvtqAkFUWPdU3DZO',
+// 				'https://res.cloudinary.com/dbuvg9h1e/image/upload/v1639027976/my-avatar-300x300.jpg'
+// 			),
+// 			(
+// 				'raju',
+// 				'raju@gmail.com',
+// 				'user',
+// 				'$2a$08$Tj9kveUf9GzZCn4l70wqdeh8Uq.FK2T6sYXcGGvtqAkFUWPdU3DZO',
+// 				'https://res.cloudinary.com/dbuvg9h1e/image/upload/v1639027976/my-avatar-300x300.jpg'
+// 			)
+// 		`)
+//  if err != nil {
+//   fmt.Println(err)
+//  } else {
+//   fmt.Println("Users Inserted ")
+//  }
+//
+//  _, err = db.Exec(`INSERT INTO products(title, description, author_id, brand_id, price, cover, tags, attributes )
+// 		VALUES (
+// 			'Redmi Note 11 Pro',
+// 			'Deno is javascript secure Runtime Engine. !!Deno is a secure and fast ',
+// 		    1,
+// 		    1,
+// 				19000.00,
+//         'images/products/huawei-enjoy-10-plus.jpg',
+// 			'["Redmi Note 11", "Redmi Note 11s", "Redmi Note 11 pro"]',
+// 			'{ "ram": 4, "rom": 64, "battery": 5000, "color": ["red", "blue", "pink", "gray"]}'
+// 		), (
+// 			'Redmi Note 11s',
+// 			'Deno is javascript secure Runtime Engine. !!Deno is a secure and fast ',
+// 		    1,
+// 		    1,
+// 				19000.00,
+//         'images/products/huawei-enjoy-10-plus.jpg',
+// 			'["Redmi Note 11", "Redmi Note 11s", "Redmi Note 11 pro"]',
+// 			'{ "ram": 4, "rom": 64, "battery": 5000, "color": ["red", "blue", "pink", "gray"]}'
+// 		)
+// 		`)
+//  if err != nil {
+//   fmt.Println(err, "-----")
+//  } else {
+//   fmt.Println("Products Inserted ")
+//  }
+//
+//
+//  _, err = db.Exec(`
+// 		INSERT INTO brands(name)
+// 			VALUES ('Samsung'), ('Nokia'), ('Apple')
+// 		`)
+//  if err != nil {
+//   fmt.Println(err, "-----")
+//  } else {
+//   fmt.Println("Products Inserted ")
+//  }
+//
+//
+// }
+//
+
 func main() {
  
- 
  config.Envload()
+ 
+ connectionStr := config.DATABASE_NAME + ":" + config.DATABASE_PASSWORD + "@tcp(" + config.DATABASE_HOST + ":" + config.DATABASE_PORT + ")/" + config.DATABASE_NAME
  
  var HOST string
  var PORT string
@@ -29,23 +164,19 @@ func main() {
  }
  
  
+
+ 
  
  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, "Hello World from path: %s\n", r.URL.Path)
  })
  
- 
- 
- 
+
  http.HandleFunc("/sql", func(w http.ResponseWriter, r *http.Request) {
- //
- // // postgres://<user>:<password>@<internal_name>:5432/<database>
- //
- //  // db, err := sql.Open("mysql", config.DATABASE_USERNAME + ":" + config.DATABASE_PASSWORD + "@" + config.DATABASE_HOST + ":" + config.DATABASE_PORT + "/" + config.DATABASE_NAME)
- //  // db, err := sql.Open("mysql", config.DATABASE_USERNAME + ":" + config.DATABASE_PASSWORD + "@" + config.DATABASE_HOST + ":" + config.DATABASE_PORT + "/" + config.DATABASE_NAME)
- //  connectionStr := os.Getenv("DATABASE_USERNAME") + ":" + os.Getenv("DATABASE_PASSWORD") + "@tcp(" + os.Getenv("DATABASE_HOST") + ":" + os.Getenv("DATABASE_PORT") + ")/" + os.Getenv("DATABASE_NAME")
   
-  connectionStr := config.DATABASE_NAME + ":" + config.DATABASE_PASSWORD + "@tcp(" + config.DATABASE_HOST + ":" + config.DATABASE_PORT + ")/" + config.DATABASE_NAME
+  // InitialCreateAllTable()
+  // InitialData()
+  //
  
   db, err := sql.Open("mysql", connectionStr)
   if err != nil {
@@ -53,14 +184,32 @@ func main() {
   }
   defer db.Close()
  
-  var result int
-  db.QueryRow(`SELECT 100 + 1 AS r`).Scan(&result)
+  userTableSql := `
+		CREATE TABLE IF NOT EXISTS users (
+		  id INT PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
+		  username VARCHAR(40) NOT NULL,
+		  email VARCHAR(50) NOT NULL UNIQUE,
+		  password VARCHAR(100) NOT NULL,
+		  avatar VARCHAR(100) NULL DEFAULT NULL,
+		  created_at DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+		  role ENUM('admin', 'user') NOT NULL DEFAULT 'user'
+		)`
+  _, err = db.Exec(userTableSql)
+  if err != nil {
+   fmt.Fprint(w, err)
+   return
+  }
  
-  fmt.Println(result)
-  
-  json.NewEncoder(w).Encode(map[string]interface{}{
-   "rows": result,
-  })
+ fmt.Println("Craeted")
+  fmt.Fprint(w, "Created")
+  // // postgres://<user>:<password>@<internal_name>:5432/<database>
+ //
+ //  // db, err := sql.Open("mysql", config.DATABASE_USERNAME + ":" + config.DATABASE_PASSWORD + "@" + config.DATABASE_HOST + ":" + config.DATABASE_PORT + "/" + config.DATABASE_NAME)
+ //  // db, err := sql.Open("mysql", config.DATABASE_USERNAME + ":" + config.DATABASE_PASSWORD + "@" + config.DATABASE_HOST + ":" + config.DATABASE_PORT + "/" + config.DATABASE_NAME)
+ //  connectionStr := os.Getenv("DATABASE_USERNAME") + ":" + os.Getenv("DATABASE_PASSWORD") + "@tcp(" + os.Getenv("DATABASE_HOST") + ":" + os.Getenv("DATABASE_PORT") + ")/" + os.Getenv("DATABASE_NAME")
+ 
+ 
+ 
  })
  
  
